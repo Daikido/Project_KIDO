33893f84f1a278eaa97391192e1e368d44a61a76
alipapado <nicksamsung880820@gmail.com>
2016-12-21 14:27:07 +0800

    component

--- Node_Project/MainProject/public/library/component.js
+++ Node_Project/MainProject/public/library/component.js
@@ -7,90 +7,53 @@ function request(path, callback) {
     req.send();
 }
 
-function Component(name) {
-    var folder = "/components/" + name + "/";
+function Component(folder, onready) {
+    this.folder = folder;
     this.info = null;
     this.script = null;
     this.html = null;
+    this.setting = null;
     var me = this;
-
-    function readyStateChanged(){
-        if(me.info!=null&&me.script!=null&&me.html!=null){
-            waitForReady.map(callback=>callback(me));
-            waitForReady = [];
-        }
+    function readyStateChanged() {
+        if (this.info != null &&
+            this.script != null &&
+            this.html != null &&
+            this.setting != null) onready();
     }
-    var waitForReady = [];
-    this.onload = function(callback){
-        waitForReady.push(callback);
-        if(this.info!=null&&this.script!=null&&this.html!=null){
-            callback(this);
-        }
-    };
 
-    request(folder + "info.json", function (info) {
+    request(folder + "/info.json", function (info) {
         me.info = JSON.parse(info);
         readyStateChanged();
     });
-    request(folder + "script.js", function (info) {
+    request(folder + "/script.js", function (info) {
         me.script = new Function(info + "build(...arguments)");
         readyStateChanged();
     });
-    request(folder + "index.html", function (info) {
+    request(folder + "/index.html", function (info) {
         me.html = info;
         readyStateChanged();
     });
+    request(folder + "/setting.html", function (info) {
+        me.setting = info;
+        readyStateChanged();
+    });
+
 
     this.build = function (setting) {
         return CreateDom("div",
             {
                 className: "component",
-                innerHTML: this.html
+                innerHTML: this.html,
+                object: this,
             }, null, ele => this.script(ele, setting));
     }
 
     this.panel = function (callback, setting) {
-        var panel = CreateDom("div", { className: "component panel" });
-        CreateDom("h1", {textContent:this.info.name}, panel);
-        CreateDom("p", {textContent:this.info.description}, panel);
-        var settingElement = {};
-        for (var i in this.info.attribute) {
-            var attr = this.info.attribute[i];
-            var row = CreateDom("div", { className: "row" }, panel);
-
-            CreateDom("label", { textContent: attr.name }, row);
-            settingElement[attr.id] = CreateDom("input", { type: attr.type }, row);
-            if(setting!=undefined) settingElement[attr.id].value = setting[attr.id];
-        }
-        var confirm = CreateDom("button", {textContent:"確認", name:"confirm"}, panel);
-        confirm.onclick = function(){
-            var result = {};
-            for(var i in settingElement) result[i] = settingElement[i].value;
-            callback(result);
-        }
-        return panel;
-    }
-
-    this.editable = function(){
-        var table = CreateDom("div",{className:"component editable"});
-        var content = CreateDom("div", {className:"content"}, table);
-        var setting = undefined;
-
-        function panelPage(){
-            content.innerHTML = "";
-            content.appendChild(me.panel(function(result){
-                setting = result;
-                buildPage()
-            }, setting));
-        }
-        function buildPage(){
-            content.innerHTML = "";
-            content.appendChild(me.build(setting));
-            CreateDom("button", {textContent:"編輯", name:"edit", onclick:function(){
-                panelPage();
-            }}, content);
-        }
-        panelPage();
-        return table;
+        return CreateDom("div",
+            {
+                className: "component",
+                innerHTML: this.setting,
+                object: this,
+            }, null);
     }
 }
