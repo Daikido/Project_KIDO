79543b9de081d81d6d3deef797dc4adcfa0e2ace
chiuhans111 <chiuhans111@gmail.com>
2016-12-29 16:22:37 +0800

    slide edit

--- newWork/framework/editor.js
+++ newWork/framework/editor.js
@@ -25,7 +25,7 @@ function hightLight(element) {
     })
 }
 addEventListener("keydown", function (e) {
-    if (e.ctrlKey && e.key == "e") {
+    if (e.ctrlKey && e.key.toLowerCase() == "e") {
         e.preventDefault();
         activeEditor();
     }
@@ -45,7 +45,7 @@ function activeEditor() {
     var panel = createDom("div", {
         className: "row editor",
 
-    }, main, ele => ele.setAttribute("layout", "200"));
+    }, main, ele => ele.setAttribute("layout", "64"));
     document.body.innerHTML = "";
 
     content.addEventListener("click", function (e) {
@@ -71,31 +71,182 @@ function activeEditor() {
         clearSelection();
     }
 
-    function setText(){
+    function setText() {
         var result = prompt("請輸入文字", selectedElement.textContent);
-        if(result!=null) selectedElement.textContent = result;
+        if (result != null) selectedElement.textContent = result;
     }
 
 
+    function drawMenu(array) {
+        panel.innerHTML = "";
+        array.map(e => {
+            createDom("div", { className: "line fadein hover" }, panel, ele => {
 
-    createDom("div", { className: "line" }, panel, ele => {
-        createDom("h1", { className: "wrap center", textContent: "關閉編輯" }, ele);
-        ele.setAttribute("color", "green");
-        ele.onclick = closeEditor
-    });
-    createDom("div", { className: "line" }, panel, ele => {
-        createDom("h1", { className: "wrap center", textContent: "文字" }, ele);
-        ele.setAttribute("color", "white");
-        ele.onclick = setText
-    });
-    createDom("div", { className: "line" }, panel, ele => {
-        createDom("h1", { className: "wrap center", textContent: "顏色" }, ele);
-        ele.setAttribute("color", "yellow");
-        ele.onclick = closeEditor
-    });
-    createDom("div", { className: "line" }, panel, ele => {
-        createDom("h1", { className: "wrap center", textContent: "刪除" }, ele);
-        ele.setAttribute("color", "red");
-        ele.onclick = deleteSelected
-    });
+                createDom("h1", { className: "wrap center", textContent: e.text }, ele, t => {
+                    t.setAttribute("color", e.tcolor || "black");
+                });
+
+                ele.setAttribute("color", e.color || "white");
+                ele.setAttribute("shade", e.shade || "500");
+
+                ele.onclick = e.onclick;
+            });
+        })
+    }
+
+
+    function menu() {
+        drawMenu([
+            { text: "X", tcolor: "red", onclick: closeEditor },
+            { text: "文字", onclick: textMenu },
+            { text: "物件", onclick: objMenu },
+        ])
+    }
+
+    function textMenu() {
+        drawMenu([
+            { text: "X", tcolor: "red", onclick: menu },
+            { text: "設定文字", onclick: setText },
+            { text: "對其方向" },
+        ])
+    }
+
+    function objMenu() {
+        drawMenu([
+            { text: "X", tcolor: "red", onclick: menu },
+            { text: "新增", onclick: objCreateMenu },
+            {
+                text: "顏色", onclick: function () {
+                    if (selectedElement)
+                        colorPanel(function (r) {
+                            if (r == null) selectedElement.setAttribute("color", "none");
+                            selectedElement.setAttribute("color", r.color);
+                            selectedElement.setAttribute("shade", r.shade);
+                        }, objMenu);
+                }
+            },
+            { text: "刪除", color: "red", onclick: deleteSelected },
+        ])
+    }
+
+    function objCreateMenu() {
+
+        
+        var target = selectedElement;
+
+        function createElement() {
+            target = selectedElement;
+
+            if(target!=null)
+            while (target.parentNode != null && !(
+                target.parentNode.classList.contains("container") ||
+                target.parentNode.classList.contains("row") ||
+                target.parentNode.classList.contains("line"))) {
+                target = target.parentNode;
+            }
+
+            if(target!=null)        
+            if(target.classList.contains("editor")) return;
+
+            var className = "row";
+            if (target.parentNode.classList.contains("row")) className = "line";
+            if (target.parentNode.classList.contains("container")) className = "page";
+            var div = createDom("div", {
+                className: className
+            });
+            return div;
+        }
+        function createElement2() {
+            var className = "row";
+            if(target==null) return createDom("div", {className:"page"});
+            if (target.classList.contains("row")) className = "line";
+            if (target.classList.contains("container")) className = "page";
+            var div = createDom("div", {
+                className: className
+            });
+            return div;
+        }
+
+
+        drawMenu([
+            { text: "X", tcolor: "red", onclick: objMenu },
+            {
+                text: "前面", onclick: function () {
+                    if(target==null) return;          
+                    var element = createElement()
+                    target.parentNode.insertBefore(element, target);
+                    setTimeout(function(){hightLight(element)}, 100);
+                }
+            },
+            {
+                text: "後面", onclick: function () {
+                    if(target==null) return;                              
+                    var element = createElement();                    
+                    if(target.nextSibling!=null)
+                    target.parentNode.insertBefore(element, target.nextSibling);
+                    else target.parentNode.appendChild(element);
+                    setTimeout(function(){hightLight(element)}, 100);              
+                }
+            },
+            {
+                text: "裡面", onclick: function () {
+                    if(target==null) target=content;                  
+                    var element = createElement2();                    
+                    target.appendChild(element);
+                    setTimeout(function(){hightLight(element)}, 100);              
+                }
+            },
+
+        ])
+    }
+
+    function colorPanel(callback, back) {
+        var buttons = [{
+            text: "X",
+            tcolor: "red",
+            onclick: back
+        }, {
+            text: "無",
+            onclick: function () {
+                callback(null);
+            }
+        }];
+        for (var i in daikido.colors) {
+            (function (color) {
+                buttons.push({
+                    color: color,
+                    onclick: function () {
+                        var buttons = [{
+                            text: "X",
+                            onclick: function () {
+                                colorPanel(callback, back)
+                            }
+                        }];
+                        if (color == "black" || color == "white") {
+                            callback({ color: color, shade: 500 })
+                        } else {
+                            ["100", "500", "900"].map(j => {
+                                buttons.push({
+                                    color: color,
+                                    shade: j,
+                                    text: j == "500" ? color : "",
+                                    onclick: function () {
+                                        callback({
+                                            color: color,
+                                            shade: j
+                                        })
+                                    }
+                                });
+                            });
+                            drawMenu(buttons);
+                        }
+                    }
+                })
+            })(i);
+        }
+        drawMenu(buttons);
+    }
+
+    menu();
+
 }
